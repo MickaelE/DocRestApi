ead39045c923ec860a1e4b46fa8d248c771d0ddc
Mickael Eriksson <eriksson.mickael@gmail.com>
2020-01-08 20:23:23 +0100

    Initial commit

---
+++ src/main/java/com/mickenet/DocRestApi/DocRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.mickenet.DocRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocRestApiApplication.class, args);
+	}
+
+}
