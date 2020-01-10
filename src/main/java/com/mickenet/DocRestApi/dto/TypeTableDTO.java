46e8f9d2273cb5c5dd880f0e9ee58a82620daa12
Mickael Eriksson <eriksson.mickael@gmail.com>
2020-01-10 20:53:28 +0100

    pojo

---
+++ src/main/java/com/mickenet/DocRestApi/dto/TypeTableDTO.java
@@ -0,0 +1,33 @@
+package com.mickenet.DocRestApi.dto;
+
+import java.sql.Timestamp;
+
+public class TypeTableDTO {
+    private int id;
+    private String typeName;
+    private Timestamp dateCreated;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public Timestamp getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Timestamp dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
